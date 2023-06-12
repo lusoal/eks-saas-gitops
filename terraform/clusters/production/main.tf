539a62f274ca21d1f5bbf73e99146fbe5989c334
lusoal <lucasdu@amazon.com>
2023-06-12 16:13:10 -0500

    adjusted karpenter iam

--- terraform/clusters/production/main.tf
+++ terraform/clusters/production/main.tf
@@ -61,52 +61,17 @@ module "eks" {
     }
     coredns = {
       most_recent = true
-      configuration_values = jsonencode({
-        computeType = "Fargate"
-        # Ensure that the we fully utilize the minimum amount of resources that are supplied by
-        # Fargate https://docs.aws.amazon.com/eks/latest/userguide/fargate-pod-configuration.html
-        # Fargate adds 256 MB to each pod's memory reservation for the required Kubernetes
-        # components (kubelet, kube-proxy, and containerd). Fargate rounds up to the following
-        # compute configuration that most closely matches the sum of vCPU and memory requests in
-        # order to ensure pods always have the resources that they need to run.
-        resources = {
-          limits = {
-            cpu = "0.25"
-            # We are targetting the smallest Task size of 512Mb, so we subtract 256Mb from the
-            # request/limit to ensure we can fit within that task
-            memory = "256M"
-          }
-          requests = {
-            cpu = "0.25"
-            # We are targetting the smallest Task size of 512Mb, so we subtract 256Mb from the
-            # request/limit to ensure we can fit within that task
-            memory = "256M"
-          }
-        }
-      })
     }
     kube-proxy = {
       most_recent = true
     }
     vpc-cni = {
       most_recent    = true
-      before_compute = true
-      configuration_values = jsonencode({
-        env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-          ENABLE_PREFIX_DELEGATION = "true"
-          WARM_PREFIX_TARGET       = "1"
-        }
-      })
     }
   }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
-  # Fargate profiles use the cluster primary security group so these are not utilized
-  create_cluster_security_group = false
-  create_node_security_group    = false
 
   manage_aws_auth_configmap = true
   aws_auth_roles = [
@@ -118,18 +83,11 @@ module "eks" {
         "system:bootstrappers",
         "system:nodes",
       ]
-    },
-  ]
-    eks_managed_node_group_defaults = {
-      iam_role_additional_policies = {
-        additional = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
-      }
     }
-
-    eks_managed_node_groups = {
-    initial = {
+  ]
+  eks_managed_node_groups = {
+    baseline-infra = {
       instance_types = ["m5.large"]
-
       min_size     = 3
       max_size     = 5
       desired_size = 3
@@ -170,17 +128,17 @@ module "eks" {
 ################################################################################
 # Flux
 ################################################################################
-module "flux_v2" {
-  source = "../../modules/flux_cd"
-  cluster_endpoint = module.eks.cluster_endpoint
-  ca = module.eks.cluster_certificate_authority_data
-  token = data.aws_eks_cluster_auth.this.token
-  git_branch = var.git_branch
-  git_username = var.git_username
-  git_password = var.git_password
-  git_url = var.git_url
-  kustomization_path = var.kustomization_path
-}
+# module "flux_v2" {
+#   source = "../../modules/flux_cd"
+#   cluster_endpoint = module.eks.cluster_endpoint
+#   ca = module.eks.cluster_certificate_authority_data
+#   token = data.aws_eks_cluster_auth.this.token
+#   git_branch = var.git_branch
+#   git_username = var.git_username
+#   git_password = var.git_password
+#   git_url = var.git_url
+#   kustomization_path = var.kustomization_path
+# }
 
 
 
