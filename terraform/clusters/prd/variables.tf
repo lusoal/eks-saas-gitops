ce82b32e892d503c685e8e313823a81214501938
lusoal <lucasdu@amazon.com>
2023-06-12 11:52:25 -0500

    Initial commit

---
+++ terraform/clusters/prd/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  default = "eks-saas-gitops"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+} 
+
+variable "vpc_cidr" {
+  default = "10.35.0.0/16"
+}
+
+variable "cluster_version" {
+  default = "1.24"
+}
+
+# variable "git_branch" {
+#   default = "main"
+# }
+
+# variable "git_password" {}
+
+# variable "git_username" {}
+
+# variable "git_url" {}
