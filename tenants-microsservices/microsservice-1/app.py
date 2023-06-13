e6415654cd204647fbc02998b83bac8e0b9e5da0
lusoal <lucasdu@amazon.com>
2023-06-13 11:48:28 -0500

    Adjusted microsservice

--- tenants-microsservices/microsservice-1/app.py
+++ tenants-microsservices/microsservice-1/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     tenant_id = os.environ.get("TENANT_ID")
-    return f"Tenant ID: {tenant_id}"
+    microsservice_version = "1.0.0"
+    return f"Tenant ID: {tenant_id} \n Microservice Version: {microsservice_version} \n"
 
 if __name__ == "__main__":
     # run in 0.0.0.0 so that it can be accessed from outside the container
