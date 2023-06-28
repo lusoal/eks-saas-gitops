06f110222a2bd1b400875776bc2d3edd1abe223f
lusoal <lucasdu@amazon.com>
2023-06-28 14:59:10 -0500

    onboarding pipeline

--- tenant-onboarding/03-tenant-onboarding.sh
+++ tenant-onboarding/03-tenant-onboarding.sh
@@ -1,4 +1,7 @@
-
+tenant_id="$1"
+tenant_model="$2"
+git_user_email="$3"
+git_user_name="$4"
 
 TENANT_TEMPLATE_FILE="TENANT_TEMPLATE.yaml"
 TENANT_MANIFEST_FILE="${tenant_id}.yaml"
@@ -7,20 +10,22 @@ TENANT_MANIFEST_FILE="${tenant_id}.yaml"
 if [ "$tenant_model" == "pooled" ]; then
     cd  /mnt/vol/eks-saas-gitops/gitops/pooled-tenants/production/config/ || exit 1
     ls
-    cp "$TENANT_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i '' "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
+    cp "$TENANT_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
     # append a new line in kustomization.yaml file using $TENANT_MANIFEST_FILE
     printf "\n  - ${TENANT_MANIFEST_FILE}\n" >> kustomization.yaml
     cd ../../../../
 
 elif [ "$tenant_model" == "siloed" ]; then
     cd  /mnt/vol/eks-saas-gitops/gitops/siloed-tenants/production/ || exit 1
-    cp "$TENANT_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i '' "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
+    cp "$TENANT_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
     printf "\n  - ${TENANT_MANIFEST_FILE}\n" >> kustomization.yaml
     cd ../../../
 fi
+
+git config --global user.email "${git_user_email}"
+git config --global user.name "${git_user_name}"
 
 git status
 git add .
 git commit -m "Adding new tenant $tenant_id in model $tenant_model"
 git push origin main
-cd .. && rm -rf eks-saas-gitops
--- tenant-onboarding/README.md
+++ tenant-onboarding/README.md
@@ -3,5 +3,5 @@
 - Create secret to be able to clone and push to the GitHub Repository
 
 ```bash
-kubectl create secret generic github-ssh-key --from-file=ssh-privatekey=/Users/lucasdu/.ssh/id_rsa_git --from-literal=ssh-privatekey.mode=0600 -nargo-workflows
+kubectl create secret generic github-ssh-key --from-file=ssh-privatekey=PATH_TO_PRIVATE_KEY --from-literal=ssh-privatekey.mode=0600 -nargo-workflows
 ```
