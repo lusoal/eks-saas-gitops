505655d326bc18aab684a8f30f37a50085b49a04
tiagoReichert <tiago@reichert.eti.br>
2023-07-27 19:23:25 -0300

    added tenants

--- tenants-microsservices/consumer/app.py
+++ tenants-microsservices/consumer/app.py
@@ -10,8 +10,8 @@ def index():
     microsservice_version = "1.0.0"
 
     message = {"tenant_id": tenant_id, "microsservice_version": microsservice_version, "microserice": "consumer"}
-
     return jsonify(message)
+
 
 if __name__ == "__main__":
     # run in 0.0.0.0 so that it can be accessed from outside the container
