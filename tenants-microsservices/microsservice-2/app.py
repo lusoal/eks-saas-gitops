59f3ce1dab743aacdc259e2bcc9f852395241f5e
tiagoReichert <tiago@reichert.eti.br>
2023-07-27 12:47:17 -0300

    SG fix

--- tenants-microsservices/microsservice-2/app.py
+++ tenants-microsservices/microsservice-2/app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # tenant_id = os.environ.get("TENANT_ID")
-    microsservice_version = "1.1.0"
-    response = {"message":f"Hello from Microservice 2 Version: {microsservice_version}"}
-    print(response)
-    return jsonify(response)
+    tenant_id = os.environ.get("TENANT_ID")
+    microsservice_version = "1.0.0"
+
+    message = {"tenant_id": tenant_id, "microsservice_version": microsservice_version, "microserice": "consumer"}
+
+    return jsonify(message)
 
 if __name__ == "__main__":
     # run in 0.0.0.0 so that it can be accessed from outside the container
