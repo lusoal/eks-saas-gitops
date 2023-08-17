e4845b6a76783729bfe7060893ffe82b3305705a
lusoal <lucasdu@amazon.com>
2023-08-17 08:36:27 -0500

    adjusted tenant onboarding script

--- tenant-onboarding/03-tenant-onboarding.sh
+++ tenant-onboarding/03-tenant-onboarding.sh
@@ -2,24 +2,38 @@ tenant_id="$1"
 tenant_model="$2"
 git_user_email="$3"
 git_user_name="$4"
+REPOSITORY_BRANCH="$5"
 
-TENANT_TEMPLATE_FILE="TENANT_TEMPLATE.yaml"
-TENANT_MANIFEST_FILE="${tenant_id}.yaml"
+MANIFESTS_PATH="/mnt/vol/eks-saas-gitops/gitops/application-plane/production/tenants"
+
+TENANT_HYBRID_TEMPLATE_FILE="TENANT_TEMPLATE_HYBRID.yaml"
+TENANT_POOL_TEMPLATE_FILE="TENANT_TEMPLATE_POOL.yaml"
+TENANT_SILO_TEMPLATE_FILE="TENANT_TEMPLATE_SILO.yaml"
+
+
+TENANT_MANIFEST_FILE="${tenant_id}-${tenant_model}.yaml"
 
 # Create new manifests for the tenant using TENANT_TEMPLATE_FILE, check if tenant_model is pooled or siloed, and update the manifests accordingly
 if [ "$tenant_model" == "pooled" ]; then
-    cd  /mnt/vol/eks-saas-gitops/gitops/pooled-tenants/production/config/ || exit 1
+    cd  $MANIFESTS_PATH || exit 1
     ls
-    cp "$TENANT_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
+    cp "$TENANT_POOL_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
     # append a new line in kustomization.yaml file using $TENANT_MANIFEST_FILE
     printf "\n  - ${TENANT_MANIFEST_FILE}\n" >> kustomization.yaml
     cd ../../../../
 
 elif [ "$tenant_model" == "siloed" ]; then
-    cd  /mnt/vol/eks-saas-gitops/gitops/siloed-tenants/production/ || exit 1
-    cp "$TENANT_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
+    cd  $MANIFESTS_PATH || exit 1
+    cp "$TENANT_SILO_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
     printf "\n  - ${TENANT_MANIFEST_FILE}\n" >> kustomization.yaml
-    cd ../../../
+    cd ../../../../
+
+elif [ "$tenant_model" == "hybrid" ]; then
+    cd  $MANIFESTS_PATH || exit 1
+    cp "$TENANT_HYBRID_TEMPLATE_FILE" "$TENANT_MANIFEST_FILE" && sed -i "s/TENANT_ID/${tenant_id}/g" "$TENANT_MANIFEST_FILE"
+    printf "\n  - ${TENANT_MANIFEST_FILE}\n" >> kustomization.yaml
+    cd ../../../../
+
 fi
 
 git config --global user.email "${git_user_email}"
@@ -28,4 +42,4 @@ git config --global user.name "${git_user_name}"
 git status
 git add .
 git commit -m "Adding new tenant $tenant_id in model $tenant_model"
-git push origin main
+git push origin $REPOSITORY_BRANCH
