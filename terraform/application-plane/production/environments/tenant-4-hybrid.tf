92a4233caf726c70b8c81243c7d451ffc77f4bee
tiagoReichert <tiago@reichert.eti.br>
2023-08-28 11:05:56 -0300

    bugfix

--- terraform/application-plane/production/environments/tenant-4-hybrid.tf
+++
@@ -1,8 +0,0 @@
-locals {}
-
-# It will deploy only the infraestructure of the siloed services
-module "hybrid_tenant_tenant-4" {
-  source = "../../../modules/tenant-apps"
-  bucket_name = "tenant-4"
-  enable_consumer = false
-}
