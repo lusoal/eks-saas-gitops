e86aad67f96bfa0b7e3be76e3606885339813ed4
lusoal <lucasdu@amazon.com>
2023-08-30 11:29:57 -0500

    Reset environment

--- terraform/application-plane/production/environments/tenant-3-hybrid.tf
+++
@@ -1,8 +0,0 @@
-locals {}
-
-# It will deploy only the infraestructure of the siloed services
-module "hybrid_tenant_tenant-3" {
-  source = "../../../modules/tenant-apps"
-  bucket_name = "tenant-3"
-  enable_consumer = false
-}
